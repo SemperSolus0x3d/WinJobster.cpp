c25feea8bab806a2e486d5c987be0a805098253d
SemperSolus0x3d <75125391+SemperSolus0x3d@users.noreply.github.com>
2023-07-14 01:31:08 +0300

    Add automated tests

---
+++ test/Tests.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cassert>
+#include <memory>
+#include <Wrappers.h>
+
+#define RUN_TEST(name) \
+    std::cout << "Running test \"" << #name << "\"" << std::endl; \
+    name();
+
+constexpr wchar_t* ExistingExecutable = L"cmd.exe";
+constexpr wchar_t* NonExistingExecutable = L"sbvhkdskdvdskjbnrmvsh.exe";
+
+
+void ItStartsAndKillsProcess()
+{
+    void* rawHandle;
+    auto errorCode = StartProcess(ExistingExecutable, L"", &rawHandle);
+
+    std::unique_ptr<void, decltype(&Cleanup)> handle(rawHandle, &Cleanup);
+
+    assert(errorCode == ErrorCode::Success);
+    assert(IsAlive(handle.get()) == true);
+
+    Kill(handle.get());
+
+    assert(IsAlive(handle.get()) == false);
+}
+
+int main()
+{
+    RUN_TEST(ItStartsAndKillsProcess);
+    return 0;
+}
